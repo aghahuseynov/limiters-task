b47676a7c582643eee0385df6da19b117ee6f432
Agha Huseynov <agahuseynov@hotmail.com>
2022-06-19 17:11:23 +0300

    authentication implement

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -18,6 +18,7 @@ describe('AppController (e2e)', () => {
   it('/ (GET)', () => {
     return request(app.getHttpServer())
       .get('/')
+      .set('Authorization', 'Agha')
       .expect(200)
       .expect('Hello World!');
   });
